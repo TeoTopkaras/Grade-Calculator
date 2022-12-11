03657c1c49090884e14f596c5da38785a2ba85fc
Teo Topkaras <101981619+TeoTopkaras@users.noreply.github.com>
2022-12-12 00:58:40 +0100

    Add files via upload

---
+++ Grade_Calculator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+# Make the width of all shells wider
+from IPython.display import display, HTML
+display(HTML("<style>.container { width:90% !important; }</style>"))
+
+
+# In[18]:
+
+
+grades = pd.read_csv('Files\grades.txt', delimiter = '\t')
+#fig = go.Figure()
+fig = px.histogram(grades, x='Grade')
+fig.update_layout(xaxis_title='Grades', yaxis_title='N',width = 900, height = 600)
+fig.show()
+
+
+# In[3]:
+
+
+for i in range(len(grades)):
+    mult = grades['ECTS']*grades['Grade']
+    sum_ECTS = np.sum(grades['ECTS'])
+    sum_mult = np.sum(mult)
+
+    
+print('Your final grade is:', round(sum_mult/sum_ECTS,2))
+
+
+# In[ ]:
+
+
+
+
